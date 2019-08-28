5a55d931395b08a06cebb8d0022330037b100c98
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2019-08-28 12:09:39 +0100

    specify download filename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function App() {
   const handleSubmit = e => {
     const event = e;
     event.preventDefault();
-
+    console.log(selectedFile.substring(0, 30));
     // in case I want to access child state from parent. use hidden target input in the form element.
     // console.log("event.target>>", event.target.fileString.value);
     console.log("submitted!");
@@ -17,12 +17,13 @@ function App() {
   const handleChange = e => {
     const file = e.target.files[0];
     console.log("file type>>>", file.type);
-    if (file.type === "text/csv") {
+    if (file.type === "text/csv" || file.type === "application/vnd.ms-excel") {
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = e => {
         const file = e.target.result;
-        selectFile(file);
+        const newFile = file.replace("application/vnd.ms-excel", "text/csv");
+        selectFile(newFile);
       };
     } else {
       selectFile("");
@@ -48,7 +49,7 @@ function App() {
         <button type="submit">Submit</button>
       </form>
       {selectedFile && (
-        <a href={selectedFile} download>
+        <a href={selectedFile} download="myFile.csv">
           Download this
         </a>
       )}
